f600610a15a3bead8b4333a1416f0f149e356291
Uladzislau Nazarau <68848418+vladNew91@users.noreply.github.com>
2022-02-27 05:44:23 +0300

    fix: url

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,2 +1,2 @@
-export const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Minsk&units=metric&appid=';
+export const URL = 'https://api.openweathermap.org/data/2.5/weather?q=Minsk&units=metric&appid=';
 export const API_KEY = '600b014cd8ba8b8635e9478d84652286';
